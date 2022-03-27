b5cb826d2b7eb7a36a67693fdc88d7637a7eb59e
Tom <tomtomnewton2005@gmail.com>
2022-03-27 22:15:03 +0100

    Add extra fields and update the class to have the toSensitiveJsonObject method to send only public information

--- src/main/kotlin/me/tomnewton/shared/Account.kt
+++ src/main/kotlin/me/tomnewton/shared/Account.kt
@@ -1,15 +1,24 @@
 package me.tomnewton.shared
 
 /**
- * A data class for an account returned from the API
- * Accounts returned from the API should not have the associated email, password, or date of birth
+ * A data class for an account
+ * Accounts returned from the API should not have the associated email, password, or date of birth, so they should be removed before sending
  * @param username The username of the account
+ * @param email The email of the account
+ * @param password The password of the account
+ * @param dateOfBirth The date of birth of the owner of the account
  */
 
 data class Account(
-    var username: String
+    val username: String,
+    val email: String,
+    val password: String,
+    val dateOfBirth: String
 ) : DataObject {
-    override fun toJsonObject() = """
-        {\"username\": \"${username}\"}
-    """.trimIndent()
+
+    override fun toJsonObject() =
+        """{username: "$username", email: $email, password: "$password", dateOfBirth: "$dateOfBirth"}"""
+
+    override fun toSensitiveJsonObject() =
+        """{username: $username}"""
 }
