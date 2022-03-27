85a5774380c7f0b5a86a5a029cf2d0973ed41f19
Tom <tomtomnewton2005@gmail.com>
2022-03-27 21:54:55 +0100

    Create an example DAO to placehold for the database

---
+++ src/main/kotlin/me/tomnewton/database/AccountDAOImpl.kt
@@ -0,0 +1,18 @@
+package me.tomnewton.database
+
+import me.tomnewton.shared.Account
+
+class AccountDAOImpl(private val accounts: MutableMap<Int, Account> = mutableMapOf()) : AccountDAO {
+
+    override fun insertAccount(id: Int, account: Account): Boolean {
+        // Return true if the previous value was null, meaning nothing would be overwritten
+        return accounts.putIfAbsent(id, account) == null
+    }
+
+    override fun getAccountById(id: Int) = accounts[id]
+
+    override fun updateAccount(id: Int, account: Account): Boolean {
+        accounts[id] = account
+        return true // This operation just doesn't fail like a database could
+    }
+}
