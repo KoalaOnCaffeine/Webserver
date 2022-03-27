643438c82cd12e680ff8afb574ee94753fdfe0c4
Tom <tomtomnewton2005@gmail.com>
2022-03-27 22:15:43 +0100

    Update the DataObject to include a toSensitiveJsonObject, so that only public information could be selected

--- src/main/kotlin/me/tomnewton/shared/DataObject.kt
+++ src/main/kotlin/me/tomnewton/shared/DataObject.kt
@@ -1,5 +1,18 @@
 package me.tomnewton.shared
 
 interface DataObject {
+
+    /**
+     * Creates a json representation of the [DataObject], with all of its fields
+     * @return a json string of the [DataObject]
+     */
+
     fun toJsonObject(): String
+
+    /**
+     * Creates a json representation of the [DataObject], which does not contain sensitive information
+     * @return a json object containing only public information
+     */
+
+    fun toSensitiveJsonObject(): String
 }
