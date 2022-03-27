76ef266f54ab767daa4a17d69a813d3280b5dec1
Tom <tomtomnewton2005@gmail.com>
2022-03-27 17:44:58 +0100

    Change Test annotations to the correct ones, and implement tests for present accounts and absent ones

---
+++ src/test/kotlin/me/tomnewton/routes/api/accounts/GetKtTest.kt
@@ -0,0 +1,30 @@
+package me.tomnewton.routes.api.accounts
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class GetKtTest {
+
+    private val elizabethOlsenAccount = Account("elizabeth_olsen")
+    private val chrisHemsworthAccount = Account("chris_hemsworth")
+
+    private val exampleDAO: AccountDAO = ExampleAccountDAO(
+        mutableMapOf(
+            0 to elizabethOlsenAccount,
+            1 to chrisHemsworthAccount
+        )
+    )
+
+    @Test
+    fun getPresentAccountTest() {
+        assertEquals(elizabethOlsenAccount, exampleDAO.getAccountById(0))
+        assertEquals(chrisHemsworthAccount, exampleDAO.getAccountById(1))
+    }
+
+    @Test
+    fun getAbsentAccountTest() {
+        assertEquals(null, exampleDAO.getAccountById(-1))
+        assertEquals(null, exampleDAO.getAccountById(2))
+    }
+
+}
