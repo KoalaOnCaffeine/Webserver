2647716ddb3711544d752f97adc23755ef586483
Tom <tomtomnewton2005@gmail.com>
2022-03-27 14:52:57 +0100

    Add API route method and create helper functions for getting parameters from a call

--- src/main/kotlin/me/tomnewton/plugins/Routing.kt
+++ src/main/kotlin/me/tomnewton/plugins/Routing.kt
@@ -1,20 +1,31 @@
 package me.tomnewton.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.util.*
-import io.ktor.util.pipeline.*
-import me.tomnewton.Messages.notFoundMessage
+import io.ktor.server.routing.*
+import me.tomnewton.routes.api.apiRoutes
+import kotlin.reflect.KProperty
 
 fun Application.configureRouting() {
 
     routing {
+        apiRoutes()
         get("/") {
             call.respondText("Hello World!")
         }
     }
+}
+
+fun ApplicationCall.parameter(name: String): String? {
+    return parameters[name]
+}
+
+fun <T> ApplicationCall.parameter(name: String, transform: (String) -> T?): T? {
+    val value = parameter(name)
+    return if (value == null) null
+    else transform(value)
+}
+
+operator fun ApplicationCall.getValue(ref: Any?, property: KProperty<*>): String? {
+    return parameter(property.name)
 }
