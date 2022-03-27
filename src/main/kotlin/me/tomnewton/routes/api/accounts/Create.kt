237da04a359c368eff7b2faa8419b3ae6fe11391
Tom <tomtomnewton2005@gmail.com>
2022-03-27 22:15:19 +0100

    Update the create method to insert the new account into the AccountDAO

--- src/main/kotlin/me/tomnewton/routes/api/accounts/Create.kt
+++ src/main/kotlin/me/tomnewton/routes/api/accounts/Create.kt
@@ -6,6 +6,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import me.tomnewton.database.AccountDAO
 import me.tomnewton.plugins.parseObject
+import me.tomnewton.shared.Account
 
 fun Route.createAccount(accountDAO: AccountDAO) {
     post("/create") {
@@ -15,13 +16,19 @@ fun Route.createAccount(accountDAO: AccountDAO) {
         val email: String by content
         val password: String by content
         val dateOfBirth: String by content
-        call.respondText(
-            """
-            username: $username
-            email: $email
-            password: $password
-            dateOfBirth: $dateOfBirth
-        """.trimIndent()
-        )
+
+        val account = Account(username, email, password, dateOfBirth)
+        val id = accountDAO.countAccounts() // If there are 0 accounts, ID=0, 1 accounts, ID=1...
+
+        val success = accountDAO.insertAccount(id, account)
+
+        if (success) {
+            // Send the account object, safe since they just made it, so they know all the information
+            call.respondText(account.toJsonObject())
+        } else {
+            // Send appropriate error
+            call.respondText("An error occurred")
+        }
+
     }
 }
