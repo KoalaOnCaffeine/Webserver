972a7b086d8df6b97a55e1232d0c4e69cb603117
Tom <tomtomnewton2005@gmail.com>
2022-03-27 21:51:40 +0100

    Send more appropriate results by using the AccountDAO

--- src/main/kotlin/me/tomnewton/routes/api/accounts/Get.kt
+++ src/main/kotlin/me/tomnewton/routes/api/accounts/Get.kt
@@ -1,13 +1,24 @@
 package me.tomnewton.routes.api.accounts
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import me.tomnewton.database.AccountDAO
 import me.tomnewton.plugins.parameter
 
 fun Route.getAccount(accountDAO: AccountDAO) {
     get("/{id}") {
         val accountID = call.parameter("id", String::toIntOrNull)
-        call.respondText(accountID?.let { "Requested the account with the id '${accountID}'" } ?: "Invalid account ID")
+        if (accountID == null) {
+            call.respondText("Invalid account ID")
+            return@get
+        }
+        val account = accountDAO.getAccountById(accountID)
+        if (account == null) {
+            call.respondText("No account found with that ID")
+            return@get
+        }
+        call.respondText(account.toJsonObject(), ContentType.Application.Json)
     }
 }
