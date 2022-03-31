e0b65bcb907fd6066bd63e0130c505ca334a4c47
Tom <tomtomnewton2005@gmail.com>
2022-03-31 21:02:51 +0100

    Create the response codes for account-based responses

---
+++ src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountResponseCodes.kt
@@ -0,0 +1,6 @@
+package me.tomnewton.shared.responses.accounts
+
+const val ACCOUNT_CREATE_SUCCESS = 0
+const val ACCOUNT_CREATE_FAIL = 1
+const val ACCOUNT_GET_SUCCESS = 2
+const val ACCOUNT_GET_FAIL = 3
