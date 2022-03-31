82d272579c6dec88f4583b58570874fb779fcf27
Tom <tomtomnewton2005@gmail.com>
2022-03-31 21:02:17 +0100

    Consolidate

--- src/main/kotlin/me/tomnewton/shared/responses/Response.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/Response.kt
@@ -3,16 +3,7 @@ package me.tomnewton.shared.responses
 import me.tomnewton.shared.DataObject
 
 open class Response(
-    private val code: Int,
-    private val message: String = "Response code :$code",
-    private val data: String = "{}"
+    private val code: Int, private val message: String = "Response code :$code", private val data: String = "{}"
 ) : DataObject {
-    override fun toJsonObject() =
-        """
-            {
-                code: $code,
-                message: "$message",
-                data: $data
-            }
-        """.trimIndent()
+    override fun toJsonObject() = """{code: $code, message: "$message", data: $data}"""
 }
