0bf7f493b4a391b8a4d67aaccdbdf31b8beed495
Tom <tomtomnewton2005@gmail.com>
2022-03-31 21:08:18 +0100

    Remove the default message for a response

--- src/main/kotlin/me/tomnewton/shared/responses/Response.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/Response.kt
@@ -3,7 +3,7 @@ package me.tomnewton.shared.responses
 import me.tomnewton.shared.DataObject
 
 open class Response(
-    private val code: Int, private val message: String = "Response code :$code", private val data: String = "{}"
+    private val code: Int, private val message: String = "", private val data: String = "{}"
 ) : DataObject {
     override fun toJsonObject() = """{code: $code, message: "$message", data: $data}"""
 }
