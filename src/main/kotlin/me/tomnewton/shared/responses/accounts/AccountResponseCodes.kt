d841dffa3f34649337a70836426d93b0db00e91f
Tom <tomtomnewton2005@gmail.com>
2022-03-31 21:43:45 +0100

    Remove conflict with the page not found error code

--- src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountResponseCodes.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountResponseCodes.kt
@@ -1,6 +1,6 @@
 package me.tomnewton.shared.responses.accounts
 
-const val ACCOUNT_CREATE_SUCCESS = 0
-const val ACCOUNT_CREATE_FAIL = 1
-const val ACCOUNT_GET_SUCCESS = 2
-const val ACCOUNT_GET_FAIL = 3
+const val ACCOUNT_CREATE_SUCCESS = 1
+const val ACCOUNT_CREATE_FAIL = 2
+const val ACCOUNT_GET_SUCCESS = 3
+const val ACCOUNT_GET_FAIL = 4
