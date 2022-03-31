821dd9855eca7424ba55b84a28888e96c020f2d8
Tom <tomtomnewton2005@gmail.com>
2022-03-31 20:35:35 +0100

    Amend the account object creation, with default empty lists (emptyList() specifically to preserve immutability so that the AccountDAO must be used #ACIDMoment)

--- src/main/kotlin/me/tomnewton/routes/api/accounts/Create.kt
+++ src/main/kotlin/me/tomnewton/routes/api/accounts/Create.kt
@@ -7,27 +7,38 @@ import io.ktor.server.routing.*
 import me.tomnewton.database.AccountDAO
 import me.tomnewton.plugins.parseObject
 import me.tomnewton.shared.Account
+import me.tomnewton.shared.responses.ErrorResponse
+import me.tomnewton.shared.responses.Response
 
 fun Route.createAccount(accountDAO: AccountDAO) {
     post("/create") {
         val content = parseObject(call.receiveText())
-
         val username: String by content
         val email: String by content
         val password: String by content
         val dateOfBirth: String by content
 
-        val account = Account(username, email, password, dateOfBirth)
+        val account = Account(
+            username,
+            email,
+            password,
+            dateOfBirth,
+            listOf(), // Should be empty by default
+            listOf(), // Should be empty by default
+            "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" // Default image url
+        )
         val id = accountDAO.countAccounts() // If there are 0 accounts, ID=0, 1 accounts, ID=1...
 
         val success = accountDAO.insertAccount(id, account)
 
         if (success) {
             // Send the account object, safe since they just made it, so they know all the information
-            call.respondText(account.toJsonObject())
+            val response = Response(0, "The account was created successfully", "{id: $id}")
+            call.respondText(response.toJsonObject())
         } else {
-            // Send appropriate error
-            call.respondText("An error occurred")
+            // Send appropriate error, such as the database failed
+            val response = ErrorResponse(0, "An error occurred while trying to create the account")
+            call.respondText(response.toJsonObject())
         }
 
     }
