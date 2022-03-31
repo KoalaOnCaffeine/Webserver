e3c787f8e0b9a0f0a34e36d0368e1de7135eecb2
Tom <tomtomnewton2005@gmail.com>
2022-03-31 20:35:52 +0100

    Create a generic error response, inheriting from Response

---
+++ src/main/kotlin/me/tomnewton/shared/responses/ErrorResponse.kt
@@ -0,0 +1,10 @@
+package me.tomnewton.shared.responses
+
+/**
+ * A typical error response, with a given code and error. The message is "An error occurred", and the specific message is under data.error
+ * @param code The code of the error
+ * @param errorDescription A description of the error
+ */
+
+class ErrorResponse(code: Int, errorDescription: String) :
+    Response(code, "An error occurred", "{error: \"$errorDescription}\"")
