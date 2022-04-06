721931788bd857c174e71a7c374dfc80db9e0f0d
Tom Newton <tomtomnewton2005@gmail.com>
2022-04-06 09:41:27 +0100

    Create the Project data class to represent a project from the database

---
+++ src/main/kotlin/me/tomnewton/database/model/Project.kt
@@ -0,0 +1,36 @@
+package me.tomnewton.database.model
+
+import me.tomnewton.shared.DataObject
+
+/**
+ * A data class for a project.
+ * @param id THe unique ID of the project
+ * @param description The description of the project
+ * @param managerIDs The managers associated with the team
+ * @param memberIDs The members associated with the team
+ * @param archived The archived state of the project
+ * @param cardIDs The cards associated with the project
+ * @param imageURL The url of the project's picture
+ */
+
+data class Project(
+    val id: Long,
+    val description: String?,
+    val managerIDs: List<Long>,
+    val memberIDs: List<Long>,
+    val imageURL: String,
+    val archived: Boolean,
+    val cardIDs: List<Long>
+) : DataObject {
+    override fun toJsonObject() = """ { id: $id, description: "$description", managerIDs: ${
+        managerIDs.joinToString(
+            prefix = "[",
+            postfix = "]"
+        )
+    }, memberIDS: ${
+        memberIDs.joinToString(
+            prefix = "[",
+            postfix = "]"
+        )
+    }, imageURL: "$imageURL", archived: $archived, cardIDs: ${cardIDs.joinToString(prefix = "[", postfix = "]")} } """
+}
