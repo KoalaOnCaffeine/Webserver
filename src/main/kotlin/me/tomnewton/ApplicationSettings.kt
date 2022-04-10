bac1e5bc7dfb5db5481300e42567dd0b88dea836
tomto <tomtomnewton2005@gmail.com>
2022-04-10 14:08:42 +0100

    Change issuer and audience because they didn't feel right

--- src/main/kotlin/me/tomnewton/ApplicationSettings.kt
+++ src/main/kotlin/me/tomnewton/ApplicationSettings.kt
@@ -2,11 +2,11 @@ package me.tomnewton
 
 object ApplicationSettings {
     // Intended audience who should use the JWT token
-    const val audience: String = "localhost"
+    const val audience: String = "http://localhost:8080/auth"
 
     // The purpose of the JWT token
     const val realm: String = "Authentication"
 
     // Who created the JWT
-    const val issuer: String = "localhost"
+    const val issuer: String = "http://localhost:8080/"
 }
