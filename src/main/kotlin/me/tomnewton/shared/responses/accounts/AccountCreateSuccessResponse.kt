a70a2492fec360560bd537b8f60589de89f2195e
tomto <tomtomnewton2005@gmail.com>
2022-04-10 14:16:21 +0100

    Include the token in the constructor so that the client can use it for future authentication

--- src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountCreateSuccessResponse.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountCreateSuccessResponse.kt
@@ -2,6 +2,6 @@ package me.tomnewton.shared.responses.accounts
 
 import me.tomnewton.shared.responses.Response
 
-class AccountCreateSuccessResponse(accountID: Long) : Response(
-    ACCOUNT_CREATE_SUCCESS, "Account created successfully", "{\"id\": $accountID}"
+class AccountCreateSuccessResponse(accountID: Long, token: String) : Response(
+    ACCOUNT_CREATE_SUCCESS, "Account created successfully", "{\"id\": $accountID, \"token\": \"$token\"}"
 )
