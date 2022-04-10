33e642329e1b33a52e59a6f51dffbeb426f29de6
tomto <tomtomnewton2005@gmail.com>
2022-04-10 14:08:02 +0100

    Add extra authentication setting so that an invalid JWT will tell the user that it is either invalid or expired

--- src/main/kotlin/me/tomnewton/plugins/Security.kt
+++ src/main/kotlin/me/tomnewton/plugins/Security.kt
@@ -2,26 +2,31 @@ package me.tomnewton.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
 import me.tomnewton.ApplicationSettings
 
 fun Application.configureSecurity() {
 
     authentication {
-        jwt {
+        jwt("auth-jwt") {
             realm = ApplicationSettings.realm
             verifier(
-                JWT
-                    .require(Algorithm.HMAC256("secret"))
-                    .withAudience(ApplicationSettings.audience)
-                    .withIssuer(ApplicationSettings.issuer)
-                    .build()
+                JWT.require(Algorithm.HMAC256("secret")) // Update this later
+                    .withAudience(ApplicationSettings.audience).withIssuer(ApplicationSettings.issuer).build()
             )
+
             validate { credential ->
-                if (credential.payload.audience.contains(ApplicationSettings.audience)) JWTPrincipal(credential.payload) else null
+                if (credential.payload.getClaim("username").asString() != "") JWTPrincipal(credential.payload) else null
             }
+
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is either invalid or has expired")
+            }
+
         }
     }
 }
