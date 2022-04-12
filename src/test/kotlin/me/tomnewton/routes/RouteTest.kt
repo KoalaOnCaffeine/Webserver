e42996ba0d17a1cc0747485e8fa75ef4324af891
Tom <tomtomnewton2005@gmail.com>
2022-04-12 17:06:14 +0100

    Comments

--- src/test/kotlin/me/tomnewton/routes/RouteTest.kt
+++ src/test/kotlin/me/tomnewton/routes/RouteTest.kt
@@ -10,23 +10,27 @@ import me.tomnewton.plugins.configureRouting
 import me.tomnewton.plugins.configureSecurity
 import me.tomnewton.plugins.configureStatusPages
 
+// Conduct a test on a route, with a given method
 internal fun test(
     method: HttpMethod, route: String, builder: HttpRequestBuilder.() -> Unit, test: HttpResponse.() -> Unit
 ) {
     val accountDAO = AccountDAOImpl()
     testApplication {
         application {
+            // Install all the plugins
             configureSecurity()
             configureRouting(accountDAO)
             configureHTTP()
             configureStatusPages()
         }
         val response = client.request {
+            // Let the builder edit the request, then set the method and route
             builder(this)
             this.method = method
             url(route)
         }
 
+        // Run the test method on the response
         test(response)
 
     }
