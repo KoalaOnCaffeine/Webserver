8f89662fb7b310cb8855e9c2c8860e639a22e84a
tomto <tomtomnewton2005@gmail.com>
2022-05-01 16:53:18 +0100

    Create the team DAO

---
+++ src/main/kotlin/me/tomnewton/database/TeamDAO.kt
@@ -0,0 +1,13 @@
+package me.tomnewton.database
+
+import me.tomnewton.database.model.Team
+
+interface TeamDAO {
+
+    fun insertTeam(team: Team): Boolean
+
+    fun getTeamById(id: Long): Team?
+
+    fun updateTeam(id: Long, team: Team): Boolean
+
+}
