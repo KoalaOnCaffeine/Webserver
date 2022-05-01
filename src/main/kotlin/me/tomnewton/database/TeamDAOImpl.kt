ebd5a09035cbc0cdad8b325f4f1ff975ab950a9e
tomto <tomtomnewton2005@gmail.com>
2022-05-01 16:55:37 +0100

    Create the implementation for the team dao

---
+++ src/main/kotlin/me/tomnewton/database/TeamDAOImpl.kt
@@ -0,0 +1,20 @@
+package me.tomnewton.database
+
+import me.tomnewton.database.model.Account
+import me.tomnewton.database.model.Team
+
+class TeamDAOImpl(private val teams: MutableMap<Long, Team> = mutableMapOf()) : TeamDAO {
+
+    override fun insertTeam(team: Team): Boolean {
+        // Return true if the previous value was null, meaning nothing would be overwritten
+        val absent = teams.putIfAbsent(team.id, team)
+        return absent == null
+    }
+
+    override fun getTeamById(id: Long) = teams[id]
+
+    override fun updateTeam(id: Long, team: Team): Boolean {
+        teams[id] = team
+        return true // todo check if there was an account
+    }
+}
