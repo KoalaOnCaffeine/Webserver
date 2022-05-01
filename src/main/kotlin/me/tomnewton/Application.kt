91fbc5ae9abc3c209ad7737931f75b2018c955e1
tomto <tomtomnewton2005@gmail.com>
2022-05-01 16:56:39 +0100

    Use the account DAO with a fake team with ID 0

--- src/main/kotlin/me/tomnewton/Application.kt
+++ src/main/kotlin/me/tomnewton/Application.kt
@@ -3,18 +3,33 @@ package me.tomnewton
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import me.tomnewton.database.AccountDAOImpl
+import me.tomnewton.database.TeamDAOImpl
+import me.tomnewton.database.model.Team
 import me.tomnewton.plugins.configureHTTP
 import me.tomnewton.plugins.configureRouting
 import me.tomnewton.plugins.configureSecurity
 import me.tomnewton.plugins.configureStatusPages
+import me.tomnewton.routes.api.accounts.defaultImage
 
 fun main() {
 
     val accountDAO = AccountDAOImpl()
+    val teamDAO = TeamDAOImpl(
+        mutableMapOf(
+            0L to Team(
+                0L,
+                "description",
+                mutableListOf(),
+                mutableListOf(),
+                mutableListOf(),
+                defaultImage
+            )
+        )
+    )
 
     embeddedServer(Netty, port = 8080, host = ApplicationSettings.domain) {
         configureSecurity()
-        configureRouting(accountDAO)
+        configureRouting(accountDAO, teamDAO)
         configureHTTP()
         configureStatusPages()
     }.start(wait = true)
