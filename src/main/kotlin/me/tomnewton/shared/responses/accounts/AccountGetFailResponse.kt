501d8749021989d3f67e3f3b5388535aa5b8cbb7
tomto <tomtomnewton2005@gmail.com>
2022-05-09 20:09:13 +0100

    Code cleanup

--- src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountGetFailResponse.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountGetFailResponse.kt
@@ -2,6 +2,6 @@ package me.tomnewton.shared.responses.accounts
 
 import me.tomnewton.shared.responses.ErrorResponse
 
-class AccountGetFailResponse() : ErrorResponse(
+class AccountGetFailResponse : ErrorResponse(
     ACCOUNT_GET_FAIL, "Either the account does not exist, or the server could not retrieve it"
 )
--- src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountLoginFailResponse.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountLoginFailResponse.kt
@@ -2,5 +2,4 @@ package me.tomnewton.shared.responses.accounts
 
 import me.tomnewton.shared.responses.ErrorResponse
 
-class AccountLoginFailResponse(reason: String) : ErrorResponse(ACCOUNT_LOGIN_FAIL, reason) {
-}
+class AccountLoginFailResponse(reason: String) : ErrorResponse(ACCOUNT_LOGIN_FAIL, reason)
