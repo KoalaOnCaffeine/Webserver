582af23a2b9d4d3fc282c9e3e8a2143212a2e253
Tom <tomtomnewton2005@gmail.com>
2022-05-11 00:14:33 +0100

    Create tests for the get route with the new route test

--- src/test/kotlin/me/tomnewton/routes/api/accounts/GetKtTest.kt
+++ src/test/kotlin/me/tomnewton/routes/api/accounts/GetKtTest.kt
@@ -1,48 +1,45 @@
 package me.tomnewton.routes.api.accounts
 
-import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import kotlinx.coroutines.runBlocking
+import me.tomnewton.database.AccountDAO
 import me.tomnewton.database.AccountDAOImpl
+import me.tomnewton.database.TeamDAO
+import me.tomnewton.database.TeamDAOImpl
 import me.tomnewton.database.model.Account
 import me.tomnewton.plugins.parseObject
 import me.tomnewton.routes.test
 import me.tomnewton.shared.responses.accounts.ACCOUNT_GET_SUCCESS
 import org.junit.Test
-import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 internal val elizabethOlsenAccount = Account(
-    0, validUsername, validPassword, validEmail, validDateOfBirth, mutableListOf(), mutableListOf(), defaultImage
+    0, validUsername, validEmail, validPassword, validDateOfBirth, mutableListOf(), mutableListOf(), defaultImage
 )
 
-internal class GetKtTest {
+class GetKtTest {
 
-    private fun get(id: Long, builder: HttpRequestBuilder.() -> Unit = {}, response: suspend HttpResponse.() -> Unit) {
-        // A new one every get, just to make sure that it always contains the account
-        val filledAccountDAO = AccountDAOImpl(mutableMapOf(elizabethOlsenAccount.id to elizabethOlsenAccount))
-        test(HttpMethod.Get, "/api/accounts/$id/", {
-            builder(this)
-        }, filledAccountDAO) {
-            runBlocking { response(this@test) }
-        }
-    }
+    private val filledAccountDAO = AccountDAOImpl(mutableMapOf(elizabethOlsenAccount.id to elizabethOlsenAccount))
 
-    private fun test(id: Long, expectedStatus: HttpStatusCode, expectedBody: String, expectedCode: Int) {
-        get(id) {
-            assertContains(contentType()?.contentType ?: "", ContentType.Application.Json.contentType)
-            assertEquals(expectedStatus, status)
+    private fun expect(
+        id: Long,
+        expectedCode: Int,
+        expectedStatusCode: HttpStatusCode = HttpStatusCode.OK,
+        expectedContentType: ContentType = ContentType.Application.Json,
+        accountDAO: AccountDAO = AccountDAOImpl(),
+        teamDAO: TeamDAO = TeamDAOImpl(),
+    ) {
+        test(HttpMethod.Get, "/api/accounts/$id", accountDAO, teamDAO, expectedContentType, expectedStatusCode) {
             val body = bodyAsText()
             val json = parseObject(body)
-            assertEquals(expectedBody, body)
-            assertEquals(expectedCode, json.getOrDefault("code", -1).toString().toIntOrNull())
+            assertEquals(expectedCode, json["code"]?.toString()?.toIntOrNull() ?: Int.MIN_VALUE)
         }
     }
 
     @Test
     fun testGetPresentAccount() {
-        test(elizabethOlsenAccount.id, HttpStatusCode.OK, elizabethOlsenAccount.toJsonObject(), ACCOUNT_GET_SUCCESS)
+        expect(
+            elizabethOlsenAccount.id, ACCOUNT_GET_SUCCESS, accountDAO = filledAccountDAO
+        )
     }
-
 }
