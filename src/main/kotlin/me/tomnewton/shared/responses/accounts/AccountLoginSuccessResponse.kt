b0e0e3607ed1a274f0bdda06d346b6d71cc9b19f
tomto <tomtomnewton2005@gmail.com>
2022-05-10 19:49:46 +0100

    Add quotations to the token field because of an error while parsing json

--- src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountLoginSuccessResponse.kt
+++ src/main/kotlin/me/tomnewton/shared/responses/accounts/AccountLoginSuccessResponse.kt
@@ -3,4 +3,4 @@ package me.tomnewton.shared.responses.accounts
 import me.tomnewton.shared.responses.Response
 
 class AccountLoginSuccessResponse(token: String) :
-    Response(ACCOUNT_LOGIN_SUCCESS, "Successfully logged in", "{token: \"$token\"}")
+    Response(ACCOUNT_LOGIN_SUCCESS, "Successfully logged in", "{\"token\": \"$token\"}")
