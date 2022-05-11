701716bde46f8e79d0d3e8fcd636519089f78a04
tomto <tomtomnewton2005@gmail.com>
2022-05-11 18:39:54 +0100

    Log when a page isn't found

--- src/main/kotlin/me/tomnewton/plugins/StatusPages.kt
+++ src/main/kotlin/me/tomnewton/plugins/StatusPages.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import me.tomnewton.shared.responses.PageNotFoundResponse
+import java.util.logging.Logger
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
@@ -12,6 +13,7 @@ fun Application.configureStatusPages() {
         // For any page which returned the not found code
         status(HttpStatusCode.NotFound) { call, _ ->
             // Respond with the notFoundMessage in a json format
+            Logger.getGlobal().info("Page not found")
             val response = PageNotFoundResponse()
             call.respondText(response.toJsonObject(), ContentType.Application.Json, HttpStatusCode.NotFound)
         }
