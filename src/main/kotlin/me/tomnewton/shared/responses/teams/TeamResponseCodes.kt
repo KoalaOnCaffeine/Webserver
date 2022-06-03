caba993c761424824456eb33fa209a81c27aecda
Tom <tomtomnewton2005@gmail.com>
2022-06-03 17:33:29 +0100

    Create team get success response

---
+++ src/main/kotlin/me/tomnewton/shared/responses/teams/TeamResponseCodes.kt
@@ -0,0 +1,3 @@
+package me.tomnewton.shared.responses.teams
+
+const val TEAMS_GET_SUCCESS = 7
---
+++ src/main/kotlin/me/tomnewton/shared/responses/teams/TeamsGetSuccessResponse.kt
@@ -0,0 +1,10 @@
+package me.tomnewton.shared.responses.teams
+
+import me.tomnewton.database.model.Team
+import me.tomnewton.shared.responses.Response
+
+class TeamsGetSuccessResponse(teams: List<Team>) : Response(TEAMS_GET_SUCCESS, data = """{"teams": ${
+    teams.joinToString(prefix = "[", postfix = "]") {
+        it.toJsonObject()
+    }
+} }""")
